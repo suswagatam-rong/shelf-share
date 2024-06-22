5c4a33803a3c9fea2412c43a10f087ede18ddf0f
Suswagatam Rong <suswagatam_rong@protonmail.com>
2024-06-22 22:04:30 +0530

    Initial commit for Shelf Share project
    Project Setup:
    - Configured Spring Boot with profiles, file upload limits, and context path in .
    - Set up PostgreSQL datasource and mail server for the development environment in .
    - Added Docker Compose configuration for PostgreSQL and MailDev services.
    - Defined project dependencies in  including Spring Boot starters, PostgreSQL driver, Lombok, JJWT, SpringDoc, and testing libraries.
    
    Key Components:
    - Spring Boot: Core framework with modules for web, data, security, mail, and validation.
    - PostgreSQL: Database setup with Docker for persistence.
    - MailDev: Development mail server configured with Docker.
    - JJWT: JSON Web Token support for authentication.
    - SpringDoc: API documentation with OpenAPI.
    
    This commit establishes the initial structure and configurations for the Shelf Share project.

---
+++ src/main/java/org/kai/shelfshare/ShelfShareApplication.java
@@ -0,0 +1,13 @@
+package org.kai.shelfshare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShelfShareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShelfShareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/kai/shelfshare/ShelfShareApplicationTests.java
@@ -0,0 +1,13 @@
+package org.kai.shelfshare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShelfShareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
